860edb9799433c7c9a62e79dc0ea627118c70ac5
andreychubin <andrey.chubin@careem.com>
2023-06-14 14:24:39 +0400

    updates

--- st_forecasting/model/losses/masked_loss.py
+++ st_forecasting/model/losses/masked_loss.py
@@ -4,6 +4,14 @@ from typing import Callable
 
 
 def masked_loss(preds: Tensor, target: Tensor, mask: Tensor, loss_fn: Callable) -> Tensor:
+    """
+    !!! Might create problems if some examples have no 'empty' tiles.
+    :param preds:
+    :param target:
+    :param mask:
+    :param loss_fn:
+    :return:
+    """
     assert preds.shape == target.shape, 'preds and target shapes do not match'
     assert len(mask.shape) == 5, 'something wrong with mask shape'
 
@@ -11,6 +19,7 @@ def masked_loss(preds: Tensor, target: Tensor, mask: Tensor, loss_fn: Callable) 
     preds_masked = preds * mask
     mask_inv = torch.where(mask == 0.0, 1.0, 0.0)
     zeros_count = torch.flatten(mask_inv, start_dim=-3, end_dim=-1).sum(2)
+    # zeros_count = torch.where(zeros_count == 0.0, 1.0, zeros_count)
     loss = loss_fn(preds_masked, target)
     loss = (loss * w * h) / zeros_count
 
